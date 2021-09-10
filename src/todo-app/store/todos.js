ce6057a2bfdc6816656f545c742545553a630b69
= <=>
2021-09-10 18:09:15 +0500

    Done with createSlice

--- src/todo-app/store/todos.js
+++ src/todo-app/store/todos.js
@@ -1,19 +1,21 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createAction, createReducer, createSlice } from "@reduxjs/toolkit";
 
-export const addTodo = createAction("addTodo");
-export const deleteTodo = createAction("deleteTodo");
-export const moveTodo = createAction("moveTodo");
-export const seedTodos = createAction("seedTodos");
-
-export default createReducer([], {
-  [addTodo.type]: (todos, action) => {
-    todos.push(action.payload);
-  },
-  [deleteTodo.type]: (todos, action) => {
-    return todos.filter((todo) => todo.id != action.payload.id);
-  },
-  [moveTodo.type]: (todos, action) => {
-    const todo = todos.find((t) => t.id == action.payload.id);
-    todo.type = action.payload.type;
+const slice = createSlice({
+  name: "todos",
+  initialState: [],
+  reducers: {
+    addTodo: (todos, action) => {
+      todos.push(action.payload);
+    },
+    deleteTodo: (todos, action) => {
+      todos = todos.filter((todo) => todo.id != action.payload.id);
+    },
+    moveTodo: (todos, action) => {
+      const todo = todos.find((t) => t.id === +action.payload.id);
+      todo.type = action.payload.type;
+    },
   },
 });
+
+export const { addTodo, deleteTodo, moveTodo } = slice.actions;
+export default slice.reducer;
